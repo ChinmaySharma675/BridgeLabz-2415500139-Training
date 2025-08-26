3ab4e0242b65df25a8da987092b4cac994736d3a
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2025-08-27 00:00:07 +0530

    commit

---
+++ ControlFlowPractices/Lvl1/Divisibleby5.java
@@ -0,0 +1,17 @@
+package ControlFlowPractices.Lvl1;
+
+import java.util.Scanner;
+public class Divisibleby5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        sc.close();
+
+        if (number % 5 == 0) {
+            System.out.println(number + " is divisible by 5.");
+        } else {
+            System.out.println(number + " is not divisible by 5.");
+        }
+    }
+    
+}
