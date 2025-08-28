66557813e0ea2970837f0f759685a1fce356e039
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2025-08-28 23:10:23 +0530

    commit

--- ControlFlowPractices/Lvl1/divide.java
+++ ControlFlowPractices/Lvl1/divide.java
@@ -1,17 +1,19 @@
 package ControlFlowPractices.Lvl1;
 
 import java.util.Scanner;
-public class Divisibleby5 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        sc.close();
 
+class divide {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
         if (number % 5 == 0) {
             System.out.println(number + " is divisible by 5.");
         } else {
             System.out.println(number + " is not divisible by 5.");
         }
+        
+        scanner.close();
     }
-    
 }
