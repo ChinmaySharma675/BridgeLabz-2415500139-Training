da47c39647cc7b588930b92a8af3ea6ce1f221f0
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2025-09-02 15:58:41 +0530

    Commit

--- ControlFlowPractices/Lvl1/counter.java
+++ ControlFlowPractices/Lvl1/counter.java
@@ -1,6 +1,7 @@
 package ControlFlowPractices.Lvl1;
 
 import java.util.Scanner;
+
 public class counter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
